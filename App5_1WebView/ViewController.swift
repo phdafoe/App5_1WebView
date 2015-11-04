9bacba2913d875fb73c9940e73bb4112fb2ee8be
phdafoe <phdafoe@gmail.com>
2015-11-04 18:00:54 +0100

    Initial Commit

---
+++ App5_1WebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App5_1WebView
+//
+//  Created by User on 4/11/15.
+//  Copyright © 2015 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
